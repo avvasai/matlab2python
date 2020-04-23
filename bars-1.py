05cfe5f913980696782c06a9585a315763131d26
keshav bantu <cy3erg33k@gmail.com>
2020-04-23 10:41:28 +0000

    Intial commit, section 10.1, No functions

---
+++ bars-1.py
@@ -0,0 +1,24 @@
+#BARS 1
+#The following routine is for the finite element solution of the free vibration of a
+#bar with a one dof per node, three noded element, and a constant area per
+#element for the eigenmode and the eigenvalue.
+#-------------------------------------------------------------------------------------
+import numpy as np
+import math
+
+#initializations
+n=150
+#number of elements
+L=1
+#lenght of rod
+t_nod=2*(n+1)
+#fixedfree case
+Fixeddof=1
+inter=L/n
+y=np.linspace(inter/2,L-inter/2,n).transpose()
+E=(210000000000)*np.ones(n)
+#youngs modulus
+Rho=7800*np.ones(n) #density
+Modeno=3;
+#modeshape found
+A=np.exp( 5+2*y*(np.sin(2*PI*y/L)) )*0.000001
